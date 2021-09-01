109c49d686076dc2918f76af086b352f3bd26af7
Brent Klein <bklein@xometry.com>
2021-09-01 14:22:00 -0400

    added ts; initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [flipResult, setFlipResult] = useState(0)
+
+  // returns 0 or 1
+  const flipCoin = (): void => setFlipResult(Math.floor(Math.random() * 2))
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {flipResult}
         </p>
-        <a
+        <button
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          onClick={flipCoin}
         >
-          Learn React
-        </a>
+          Flip Coin
+        </button>
       </header>
     </div>
   );
