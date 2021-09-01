bc718e15673018f9169c6f402817739884adaae5
Brent Klein <bklein@xometry.com>
2021-09-01 17:03:12 -0400

    Added ability to reflip wins

--- src/App.tsx
+++ src/App.tsx
@@ -10,24 +10,46 @@ function App() {
   // returns 0 or 1
   const flipCoin = (): number => Math.floor(Math.random() * 2)
 
-  const flipCoins = (): void => {
+  const flipCoins = (n :number): [number, JSX.Element[]] => {
     if (!haveThumb) {
       const r: number[] = []
-      for (let i = 0; i < numberOfCoins; i++) {
+      for (let i = 0; i < n; i++) {
         r.push(flipCoin())
       }
       const totalWins = r.reduce((sum, current) => sum + current, 0)
-      setResults(r.map(v => (<span>{v}</span>)))
-      setFlipResult(totalWins)
+      return [totalWins, r.map(v => (<span>{v}</span>))]
     } else {
       const r: [number, number][] = []
-      for (let i = 0; i < numberOfCoins; i++) {
+      for (let i = 0; i < n; i++) {
         r.push([flipCoin(), flipCoin()])
       }
       const totalWins = r.reduce((sum, current) => sum + (current[0] || current[1] ? 1 : 0), 0)
-      setResults(r.map(vs => (<span>({vs[0]}, {vs[1]})</span>)))
-      setFlipResult(totalWins)
+      return [totalWins, r.map(vs => (<span>({vs[0]}, {vs[1]})</span>))]
     }
+  }
+
+  const reflipWins = (coinsToFlip: number = numberOfCoins): [number, JSX.Element[]] => {
+    const [wins, res] = flipCoins(coinsToFlip)
+    let totalWins = wins
+    let totalRes = res
+    if (wins > 0) {
+      const [newWins, newRes] = reflipWins(wins)
+      totalWins += newWins
+      totalRes = totalRes.concat(newRes)
+    }
+    return [totalWins, totalRes]
+  }
+
+  const flipManyce = (): void => {
+    const [totalWins, r] = reflipWins()
+    setResults(r)
+    setFlipResult(totalWins)
+  }
+
+  const flipOnce = (): void => {
+    const [totalWins, r] = flipCoins(numberOfCoins)
+    setResults(r)
+    setFlipResult(totalWins)
   }
 
   const handleNumberChange = (event: React.ChangeEvent<HTMLInputElement>): void => setNumberOfCoins(event.target.valueAsNumber)
@@ -54,9 +76,15 @@ function App() {
           onChange={handleCheckboxChange} />
         <button
           className="App-link"
-          onClick={flipCoins}
+          onClick={flipOnce}
         >
           Flip Coin{(numberOfCoins > 1) && (<span>s</span>)}
+        </button>
+        <button
+          className="App-link"
+          onClick={flipManyce}
+        >
+          Reflip Wins
         </button>
         <p>Results</p>
         {results}
